1dd19665ddbd7208a95122fa3e2386b62371d573
kumail-r <robotrage2009@live.com>
2023-03-02 20:22:38 +0000

    submits encrypted fields

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from flask import Blueprint
 from flask import Flask, jsonify, render_template, request
 from .blockchain import Blockchain
 from .blockchain import Block
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+import base64
 
 # Creating the Web
 # App using flask
@@ -38,13 +42,50 @@ def add_block_site():
 
 @main.route('/adding_block', methods=['POST'])
 def add_block():
-    import time
-    diagnosis = request.form.get('diagnosis')
-    doctor = request.form.get('doctor')
-    symptoms = request.form.get('symptoms')
-    treatment = request.form.get('treatment')
-    prescription = request.form.get('prescription')
+    public_key = request.form.get('publickey').replace("\\n", "\n")
+    public_key = serialization.load_pem_public_key(public_key.encode())
 
+    diagnosis = base64.b64encode(public_key.encrypt(
+		request.form.get('diagnosis').encode(),
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+	)).decode('utf-8')
+    doctor = base64.b64encode(public_key.encrypt(
+		request.form.get('doctor').encode(),
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+	)).decode('utf-8')
+    symptoms = base64.b64encode(public_key.encrypt(
+		request.form.get('symptoms').encode(),
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+	)).decode('utf-8')
+    treatment = base64.b64encode(public_key.encrypt(
+		request.form.get('treatment').encode(),
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+	)).decode('utf-8')
+    prescription = base64.b64encode(public_key.encrypt(
+		request.form.get('prescription').encode(),
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+	)).decode('utf-8')
+    
     blockchain.add_new_transaction(1)
     print(blockchain.mine(diagnosis, doctor, symptoms, treatment, prescription))
     return render_template('add_block.html')
